18780ee7f840f452a7a0afa8f63cec9de6c26ec5
Matthew <mattdilandro247@gmail.com>
2015-05-13 16:34:39 -0400

    asdaD

--- MemoryandPaging.c
+++ MemoryandPaging.c
@@ -19,6 +19,7 @@ struct PageTable{
 	int * PageNumber;      //array of page number
 	int * MemoryLocation;  //indices stored parallel to PageNumber
 	int offset;
+	int bytesincache; //Says the amount of bytes in cache
 	clock_t PageTableUseTime;
 };
 
@@ -48,6 +49,10 @@ struct PageTable * CreatePageTable(){
 struct Memory * CreateMemory(){
 	struct Memory *  MemoryBank = malloc(sizeof(struct Memory));
 	MemoryBank->frames = calloc(numFrames, frameSize);
+	int i;
+	for(i=0;i<numFrames;i++){
+		MemoryBank->frames[i] = "-1";
+	}
 	MemoryBank->AllPageTables = malloc(AllPageTablesSize);
 	MemoryBank->freeFrames = numFrames;
 	MemoryBank->numPageTables = 0;
@@ -58,74 +63,93 @@ struct Memory * CreateMemory(){
     //~ int maxValIndex = INT_MAX;
 //~ }
 
-void DeleteOldest(struct Memory* MemoryBank, int deletions){
-    int removedframes;
-    struct PageTable ** Temp = calloc(MemoryBank->numPageTables,sizeof(struct PageTable));
-    int i;
-    for (i = 0; i < MemoryBank->numPageTables; i++){
-        Temp[i] = MemoryBank->AllPageTables[i];
+void Merge(struct PageTable **pt, struct PageTable **Lpt, int leftCount, struct PageTable **Rpt, int rightCount) {
+    int i,j,k;
+    i = 0; j = 0; k =0;
+    while(i<leftCount && j< rightCount) {
+        if(Lpt[i]->PageTableUseTime  > Rpt[j]->PageTableUseTime) pt[k++] = Lpt[i++];
+        else pt[k++] = Rpt[j++];
     }
-    //Sort the PageTables
+    while(i < leftCount) pt[k++] = Lpt[i++];
+    while(j < rightCount) pt[k++] = Rpt[j++];
+}
+void MergeSort(struct PageTable **pt, int n) {
+    int mid,i;
+    struct PageTable **L = malloc(sizeof(struct PageTable*)*((n/2) + 1));
+    struct PageTable **R = malloc(sizeof(struct PageTable*)*((n/2) + 1));
+    if(n < 2) return;
     
-    for(i=0;i<numPages;i++){
-	if(Temp[0]->PageNumber[i] == -1){
-		//end of page table
-	}
-	else{
-		//Delete it from cache
-		MemoryBank->frames[Temp[0]->MemoryLocation[i]] = "-1";
-		Temp[0]->PageNumber[i] = -1;
-		Temp[0]->MemoryLocation[i] = -1;
-		removedframes = removedframes + 1;
-	}
-    }
-    if(removedframes >= deletions){
-		//return EXIT_SUCCESS;
-    }
-    //See if the removed frames where enough if not rerun the function
+    mid = n/2; 
+    L = malloc(mid*sizeof(struct PageTable));
+    R = malloc((n- mid)*sizeof(struct PageTable));
+    
+    for(i = 0;i<mid;i++) L[i] = pt[i];
+    for(i = mid;i<n;i++) R[i-mid] = pt[i]; 
+    
+    MergeSort(L,mid); 
+    MergeSort(R,n-mid); 
+    Merge(pt,L,mid,R,n-mid);
+    free(L);
+    free(R);
 }
 
-//~ int* findEmpty(struct Memory* MemoryBank, int framesNecessary){
-    //~ if (framesNecessary > MemoryBank->numFiles){
-        //~ framesNecessary = MemoryBank->numFiles;
-    //~ }
-    //~ if (framesNecessary > MemoryBank->freeFrames){
-        //~ //delete that many files
-        //~ DeleteOldest(MemoryBank, (framesNecessary - MemoryBank->freeFrames));
-    //~ }
-    
-    
-    //~ int* empties = malloc(sizeof(int)*framesNecessary);
-    //~ int emptyIndex = 0;
-    
-    //~ int i;
-    //~ for(i = 0; i < numFrames; i++){
-        //~ if (framesNecessary-1 == emptyIndex){
-            //~ break;
-        //~ }
-        
-        //~ if (MemoryBank->frames[i]){ //go to each string and check if it's empty
-            //~ if (MemoryBank->frames[i][0] == '\0'){
-                //~ empties[emptyIndex] = i;
-            //~ }
-        //~ }
-    //~ }
-    
-    //~ return empties;
-//~ }
+void DeleteOldest(struct Memory* MemoryBank, int deletions){
+	int removedframes;
+	struct PageTable ** Temp = calloc(MemoryBank->numPageTables,sizeof(struct PageTable));
+	int i;
+	while(removedframes != deletions || removedframes < deletions){
+		for (i = 0; i < MemoryBank->numPageTables; i++){
+			Temp[i] = MemoryBank->AllPageTables[i];
+		}
+		MergeSort(Temp, MemoryBank->numPageTables);
+		for(i=0;i<numPages;i++){
+			if(Temp[0]->PageNumber[i] == -1){
+				break;
+			}
+			else{
+				//Delete it from cache
+				MemoryBank->frames[Temp[0]->MemoryLocation[i]] = "-1";
+				Temp[0]->PageNumber[i] = -1;
+				Temp[0]->MemoryLocation[i] = -1;
+				MemoryBank->freeFrames = MemoryBank->freeFrames - 1;
+				removedframes = removedframes + 1;
+			}
+		}
+		for (i = 0; i < MemoryBank->numPageTables; i++){
+			if(strcmp(Temp[0]->Name,MemoryBank->AllPageTables[i]->Name) == 0){
+				int j;
+				for (j = 0; j < MemoryBank->numPageTables; j++){
+					MemoryBank->AllPageTables[j] = MemoryBank->AllPageTables[j+1];
+				}
+				MemoryBank->numPageTables = MemoryBank->numPageTables-1;
+			}
+		}
+	}
+}
 
-//store(Memory* MemoryBank, string filename, int bytes, string data)
-//read(Memory* MemoryBank, int offset, int length)
-//deleteOldest(Memory* MemoryBank, int numToFree)
-
-//~ int FileInCache(char * FileName, struct Memory * MemoryBank){
-	//~ int i;
-	//~ for(i=0;i<MemoryBank->numFiles;i++){
-		//~ if(strcmp(MemoryBank->Fille
-		
-	//~ }
-	//~ return 0;
-//~ }
+int* findEmpty(struct Memory* MemoryBank, int framesNecessary){
+	if (framesNecessary > MemoryBank->freeFrames){
+		//Find the first ones in memory
+	}
+	if (framesNecessary < MemoryBank->freeFrames){
+		//delete that many files
+		DeleteOldest(MemoryBank, framesNecessary - MemoryBank->freeFrames);
+	}
+	int * framesreturned = calloc(1024,sizeof(int));
+	int size;
+	size = 0;
+	int i;
+	for(i=0;i<numFrames;i++){
+		if(strcmp(MemoryBank->frames[i],"-1")==0){
+			framesreturned[size] = i;
+			size = size + 1;
+		}
+		if(size == framesNecessary){
+			break;
+		}
+	}
+	return framesreturned;
+}
 
 //------------------------------------------------------------
 
