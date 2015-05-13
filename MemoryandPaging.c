374748b45f544ed7818bde9bba4d880510a74d8a
JimmyJimJimJammers <mccarthyj10@aol.com>
2015-05-13 13:59:35 -0400

    do whatever you want fucker

--- MemoryandPaging.c
+++ MemoryandPaging.c
@@ -6,16 +6,22 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/queue.h>
+#include <time.h>
+#include <limits.h>
 
 #define AllFilesSize 1024
 #define numFrames 32
 #define frameSize 1024
+#define numPages 4
 
 struct File{
 	char * Name;
-	int ** PageNumber;
-	int ** MemoryLocation;
+	int * PageNumber;      //array of page number
+	int * MemoryLocation;  //indices stored parallel to PageNumber
+    int offset;
 	int ByteLength;
+    clock_t fileCreationTime;
+    clock_t fileModificationTime;
 };
 
 struct Memory{ 
@@ -23,39 +29,85 @@ struct Memory{
 	struct File ** AllFiles;
     int freeFrames;
     int numFiles;
-    
 };
+
+struct File * CreateFile(){
+    struct File * cacheFile = malloc(sizeof(struct File));
+    cacheFile->PageNumber = calloc(numPages, sizeof(int));
+    cacheFile->MemoryLocation = calloc(numPages, sizeof(int));
+    cacheFile->offset = 0;
+    cacheFile->ByteLength = 0;
+    cacheFile->fileCreationTime = clock();                            //remind miguel to make clock(0);
+    cacheFile->fileModificationTime = clock();
+    
+    return cacheFile;
+}
 
 struct Memory * CreateMemory(){
 	struct Memory *  MemoryBank = malloc(sizeof(struct Memory));
 	MemoryBank->frames = calloc(numFrames, frameSize);
 	MemoryBank->AllFiles = malloc(AllFilesSize);
-    freeFrames = numFrames;
-    numFiles = 0;
+    MemoryBank->freeFrames = numFrames;
+    MemoryBank->numFiles = 0;
+    
 	return MemoryBank;
 }
 
-int* findEmpty(Memory* MemoryBank, int framesNecessary)
+int findMaxElement(struct Memory* MemoryBank, int* indices) //finds max element in an array and returns that index
 {
+    int maxValIndex = INT_MAX;
+}
+
+void DeleteOldest(struct Memory* MemoryBank, int deletions)
+{
+    int* removals = malloc(sizeof(int)*deletions);
+    
+    //go through and fill removals with things and record the maximum element of it and the minimum element
+    
+    //as we look at new things if they are smaller than the max element replace the max element with it, if it is smaller than the min element replace the min element with it and update min
+    
+    int i;
+    for (i = 0; i < MemoryBank->numFiles; i++)
+    {
+        
+    }
+}
+
+int* findEmpty(struct Memory* MemoryBank, int framesNecessary)
+{
+    if (framesNecessary > MemoryBank->numFiles)
+    {
+        framesNecessary = MemoryBank->numFiles;
+    }
+    
+    if (framesNecessary > MemoryBank->freeFrames)
+    {
+        //delete that many files
+        DeleteOldest(MemoryBank, (framesNecessary - MemoryBank->freeFrames));
+    }
+    
+    
     int* empties = malloc(sizeof(int)*framesNecessary);
     int emptyIndex = 0;
     
     int i;
     for(i = 0; i < numFrames; i++)
     {
-        if (framesNecessary == emptyIndex)
+        if (framesNecessary-1 == emptyIndex)
         {
             break;
         }
         
-        if (MemoryBank.frames[i]) //go to each string and check if it's empty
+        if (MemoryBank->frames[i]) //go to each string and check if it's empty
         {
-            if (frames[i][0] == '\0')
+            if (MemoryBank->frames[i][0] == '\0')
             {
                 empties[emptyIndex] = i;
             }
         }
     }
+    
+    return empties;
 }
 
 //store(Memory* MemoryBank, string filename, int bytes, string data)
@@ -67,7 +119,8 @@ int* findEmpty(Memory* MemoryBank, int framesNecessary)
 
 
 
-int main( int argc, char *argv[] ){
+int main( int argc, char *argv[] )
+{
 	struct Memory *  MemoryBank = CreateMemory();
 	return EXIT_SUCCESS;
 }
