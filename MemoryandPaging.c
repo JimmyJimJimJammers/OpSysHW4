939f3640270a36298e922dcc28adbae3e4afa936
Matthew <mattdilandro247@gmail.com>
2015-05-13 00:36:53 -0400

    change shit

--- MemoryandPaging.c
+++ MemoryandPaging.c
@@ -6,9 +6,27 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define AllFilesSize 1024
+
+struct File{
+	char * Name;
+	int ** PageNumber;
+	int ** MemoryLocation;
+};
+
 struct Memory{ 
-	char ** Cache;
+	char ** Frames;
+	struct File ** AllFiles;
+};
+
+struct Memory * CreateMemory(){
+	struct Memory *  MemoryBank = malloc(sizeof(struct Memory));
+	MemoryBank->Frames = calloc(32,1024);
+	MemoryBank->AllFiles = malloc(AllFilesSize);
+	return MemoryBank;
 }
+
 int main( int argc, char *argv[] ){
-	
+	struct Memory *  MemoryBank = CreateMemory();
+	return EXIT_SUCCESS;
 }
